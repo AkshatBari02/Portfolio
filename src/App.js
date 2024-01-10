b87332a8066e6ef43c0fc1e6528f0f2b2f5330eb
AkshatBari02 <akshatbari.ab@gmail.com>
2024-01-10 14:51:59 +0530

    commit made

--- src/App.js
+++ src/App.js
@@ -1,21 +1,15 @@
 import {BrowserRouter,Routes,Route} from "react-router-dom"
 import React from "react";
-import DrawerAppBar from "./components/common/header";
-import Footer from "./components/common/footer";
-import LogIn from "./components/loginPage";
-import Home from "./components/home";
-import ActionAreaCard from "./components/homePage";
-// import './App.css';
+import WeatherApp from "./tempapp";
+import "./App.css";
+
 
 function App() {
   return (
     <BrowserRouter>
-    <DrawerAppBar/>
     <Routes>
-      <Route path='/' element={<Home/>}/>
-      <Route path='/login' element={<LogIn/>}></Route>
+      <Route path='/' element={<WeatherApp/>}/>
     </Routes>
-    <Footer/>
     </BrowserRouter>
   );
 }
